2beb719baf578a2b54ca7a8ad6dbd9f295a399c7
Emil Priver <hello@emilpriver.com>
2019-02-07 12:45:50 +0100

    added try catch

--- inlamning_4.ps1
+++ inlamning_4.ps1
@@ -1,28 +1,110 @@
-﻿#computers
-$computers = @('Klient01', 'Klient02','Klient03')
-$password = "Test123" | ConvertTo-SecureString -asPlainText -Force
-$username = "grupp04.lab\Emil"
+#computers
+$config  = Get-Content -Raw -Path config.json | ConvertFrom-Json
+$computers = @($config.clients)
+$password = $config.user.password | ConvertTo-SecureString -asPlainText -Force
+$username =  $config.user.domain+'\'+$config.user.username
 $credential = New-Object System.Management.Automation.PSCredential($username,$password) 
 
 foreach ($computer in $computers) {
     #bios
-    Get-WmiObject win32_bios -ComputerName $computer  -Credential $credential | Select-Object Version, Manufacturer, releasedate | Export-Csv -Path "output\($computer)_Bios.csv"
+    if($config.settings.bios){
+        try 
+        {
+            Get-WmiObject win32_bios -ComputerName $computer  -Credential $credential -ErrorAction Stop | Select-Object Version, Manufacturer, releasedate -ErrorAction Stop | Export-Csv -Path "output\($computer)_Bios.csv" -ErrorAction Stop
+        }
+        catch 
+        {
+            write-host 'Failed to fetch bios'
+        }
+    }
     #motherboard
-    Get-WmiObject Win32_BaseBoard -ComputerName $computer -Credential $credential | Select-Object Manufacturer, Product , Serialnumber | Export-Csv -Path "output\($computer)_Mboard.csv"
+    if($config.settings.baseBord){
+        try 
+        {
+            Get-WmiObject Win32_BaseBoard -ComputerName $computer -Credential $credential -ErrorAction Stop | Select-Object Manufacturer, Product , Serialnumber -ErrorAction Stop | Export-Csv -Path "output\($computer)_Mboard.csv" -ErrorAction Stop
+        }
+        catch 
+        {
+            write-host 'Failed fetching motherboard'
+        }
+    }
     #CPU
-    Get-WmiObject  win32_processor -ComputerName $computer  -Credential $credential | Select-Object Name,processortype, Numberofcores, numberoflogicalprocessors | Export-csv -Path "output\($computer)_Cpu.csv"
+    if($config.settings.proccessor){
+        try 
+        {
+            Get-WmiObject  win32_processor -ComputerName $computer  -Credential $credential -ErrorAction Stop | Select-Object Name,processortype, Numberofcores, numberoflogicalprocessors | Export-csv -Path "output\($computer)_Cpu.csv"
+        }
+        catch 
+        {
+            write-host 'Could not fetch proccessor'
+        }
+    }
     #ram
-    Get-WmiObject  win32_physicalmemory -ComputerName $computer  -Credential $credential  | Select-Object devicelocator , capacity,manufacturer, partnumber, serialnumber | export-csv -Path "output\($computer)_Ram.csv"
+    if($config.settings.physical_memory){
+        try 
+        {
+            Get-WmiObject  win32_physicalmemory -ComputerName $computer  -Credential $credential -ErrorAction Stop  | Select-Object devicelocator , capacity,manufacturer, partnumber, serialnumber | export-csv -Path "output\($computer)_Ram.csv"
+        }
+        catch 
+        {
+            write-host 'Could not fetch RAM'
+        }
+    }
     #HDD
-    Get-WmiObject Win32_DiskDrive -ComputerName $computer  -Credential $credential | Select-Object Manufacturer, Model,SerialNumber,Size | Export-Csv -Path "output\($computer)_hdd.csv"
-    #Logic disks
-    Get-WmiObject win32_logicaldisk -ComputerName $computer  -Credential $credential | Select-Object DeviceID, Filesystem, size, freespace | Export-Csv -Path "output\($computer)_hddlogic.csv"
+    if($config.settings.diskdrive){
+        try 
+        {
+            Get-WmiObject Win32_DiskDrive -ComputerName $computer  -Credential $credential -ErrorAction Stop | Select-Object Manufacturer, Model,SerialNumber,Size | Export-Csv -Path "output\($computer)_hdd.csv"
+        }
+        catch 
+        {
+            write-host 'Could not fetch HDD'
+        }
+    }
+    #logic disks
+    if($config.settings.logicaldisk){
+        try 
+        {
+            Get-WmiObject win32_logicaldisk -ComputerName $computer  -Credential $credential -ErrorAction Stop | Select-Object DeviceID, Filesystem, size, freespace | Export-Csv -Path "output\($computer)_hddlogic.csv"
+        }
+        catch 
+        {
+            write-host 'Could not fetch Loguic disks'
+        }
+    }
     #windows updates
-    Get-WmiObject win32_quickfixengineering -ComputerName $computer  -Credential $credential  | Select-Object description,hotfixid | Export-Csv -Path "output\($computer)_wupdate.csv"
+    if($config.settings.windows_update){
+        try 
+        {
+            Get-WmiObject win32_quickfixengineering -ComputerName $computer  -Credential $credential -ErrorAction Stop  | Select-Object description,hotfixid | Export-Csv -Path "output\($computer)_wupdate.csv"
+        }
+        catch 
+        {
+            write-host 'Failed to fetch Windows Updates'
+        }
+    }
     #Software
-    Get-WmiObject win32_product -ComputerName $computer  -Credential $credential | Select-Object name | Export-Csv -Path "output\($computer)_mjukvara.csv"
+    if($config.settings.software){
+        try 
+        {
+            Get-WmiObject win32_product -ComputerName $computer  -Credential $credential -ErrorAction Stop | Select-Object name | Export-Csv -Path "output\($computer)_mjukvara.csv"    
+        }
+        catch 
+        {
+            write-host 'Failed to fetch Softwares'
+        }
+    }
     #ip
-    Get-WmiObject win32_networkadapterconfiguration -ComputerName $computer  -Credential $credential | Select-Object ipaddress,defaultgateway,dnsdomain,dhcpenabled | Export-Csv -Path "output\($computer)_IP.csv"
+    if($config.settings.ip) {
+        try 
+        {
+            Get-WmiObject win32_networkadapterconfiguration -ComputerName $computer  -Credential $credential -ErrorAction Stop | Select-Object ipaddress,defaultgateway,dnsdomain,dhcpenabled | Export-Csv -Path "output\($computer)_IP.csv"
+        }
+        catch 
+        {
+            write-host 'Faield to fetch IP'
+        }
+    }
 
     write-host $computer ' Done'
 
