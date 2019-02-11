74e4912c2fecae488d884e213a6be4968e2cc131
Emil Priver <hello@emilpriver.com>
2019-02-11 16:05:41 +0100

    Update inlamning_4.ps1

--- inlamning_4.ps1
+++ inlamning_4.ps1
@@ -1,121 +1,199 @@
 #computers
-$config  = Get-Content -Raw -Path config.json | ConvertFrom-Json
+#HOW TO USE
+#Define witch data you want in the config.json file and name of the computers you want information from.
+#The script will get all info and print it to the output mapp.
+
+
+$config = Get-Content -Raw -Path config.json | ConvertFrom-Json
 $computers = @($config.clients)
 $password = $config.user.password | ConvertTo-SecureString -asPlainText -Force
-$username =  $config.user.domain+'\'+$config.user.username
-$credential = New-Object System.Management.Automation.PSCredential($username,$password) 
+$username = $config.user.domain + '\' + $config.user.username
+$credential = New-Object System.Management.Automation.PSCredential($username, $password) 
 
+#check fiels
+write-host "Looking for needed files, I will create the files i don't find that I need"
+#BIOS
+if ($config.settings.bios) {
+    if ((test-path -Path "output/Bios.csv") -eq $false) {
+        New-Item -Path "output/" -Name "Bios.csv" -ItemType "file"
+        "{0},{1},{2},{3}" -f 'Computer name', 'Version', 'Manufacturer', 'Releasedate' | add-content -Path "output\Bios.csv"         
+    }
+}
+#MotherBoard
+if ($config.settings.baseBord) {
+    if ((test-path -Path "output/Mboard.csv") -eq $false) {
+        New-Item -Path "output/" -Name "Mboard.csv" -ItemType "file"
+        "{0},{1},{2},{3}" -f 'Computer name',  'Manufacturer', 'Product' , 'SerialNumber' | add-content -Path "output\Mboard.csv"         
+    }
+}
+#CPU
+if ($config.settings.proccessor) {
+    if ((test-path -Path "output/Cpu.csv") -eq $false) {
+        New-Item -Path "output/" -Name "Cpu.csv" -ItemType "file"
+        "{0},{1},{2},{3},{4}" -f 'Computer name',  'Name', 'processortype' , 'Numberofcores', 'numberoflogicalprocessors' | add-content -Path "output\Cpu.csv"         
+    }
+}
+
+
+#Ram
+if ($config.settings.physical_memory) {
+    if ((test-path -Path "output/Ram.csv") -eq $false) {
+        New-Item -Path "output/" -Name "Ram.csv" -ItemType "file"
+        "{0},{1},{2},{3},{4},{5}" -f 'Computer name',  'devicelocator', 'capacity' , 'manufacturer', 'partnumber' , 'serialnumber' | add-content -Path "output\Ram.csv"         
+    }
+}
+
+#HDD
+if ($config.settings.diskdrive) {
+    if ((test-path -Path "output/Hdd.csv") -eq $false) {
+        New-Item -Path "output/" -Name "Hdd.csv" -ItemType "file"
+        "{0},{1},{2},{3},{4}" -f 'Computer name',  'Manufacturer', 'Model' , 'SerialNumber', 'Size' | add-content -Path "output\Hdd.csv"         
+    }
+}
+#Logic disks
+if ($config.settings.logicaldisk) {
+    if ((test-path -Path "output/Hddlogic.csv") -eq $false) {
+        New-Item -Path "output/" -Name "Hddlogic.csv" -ItemType "file"
+        "{0},{1},{2},{3},{4}" -f 'Computer name',  'DeviceID', 'Filesystem' , 'size', 'freespace' | add-content -Path "output\Hddlogic.csv"         
+    }
+}
+#Windows Updates
+if ($config.settings.windows_update) {
+    if ((test-path -Path "output/Wupdate.csv") -eq $false) {
+        New-Item -Path "output/" -Name "Wupdate.csv" -ItemType "file"
+        "{0},{1},{2}" -f 'Computer name',  'description', 'hotfixid' | add-content -Path "output\Wupdate.csv"         
+    }
+}
+#Softwares
+if ($config.settings.software) {
+    if ((test-path -Path "output/Software.csv") -eq $false) {
+        New-Item -Path "output/" -Name "Software.csv" -ItemType "file"
+        "{0},{1}" -f 'Computer name',  'name'  | add-content -Path "output\Software.csv"         
+    }
+}
+#IP
+if ($config.settings.ip) {
+    if ((test-path -Path "output/IP.csv") -eq $false) {
+        New-Item -Path "output/" -Name "IP.csv" -ItemType "file"
+        "{0},{1},{2},{3},{4}" -f 'Computer', 'IP Adrress', 'Default IP Gateway', 'DNS Domain', 'DHCP Enabled' | add-content -Path "output\IP.csv"         
+    }
+}
+
+#Foreach computer that the user want to get information from
 foreach ($computer in $computers) {
-    #bios
-    if($config.settings.bios){
-        try 
-        {
-            Get-WmiObject win32_bios -ComputerName $computer  -Credential $credential -ErrorAction Stop | Select-Object Version, Manufacturer, releasedate -ErrorAction Stop | Export-Csv -Path "output\($computer)_Bios.csv" -ErrorAction Stop
+    #check if computer is up
+    if (Test-Connection -ComputerName $computer -Count 1 -Quiet ) {
+        #If computers name = the selected computer
+        if (!([string]$env:computername -eq [string]$computer.ToUpper())) {
+            $auth = @{
+                ComputerName = $computer 
+                Credential   = $credential
+            }
         }
-        catch 
-        {
-            write-host 'Failed to fetch bios'
+        else {
+            $auth = ''
         }
-    }
-    #motherboard
-    if($config.settings.baseBord){
-        try 
-        {
-            Get-WmiObject Win32_BaseBoard -ComputerName $computer -Credential $credential -ErrorAction Stop | Select-Object Manufacturer, Product , Serialnumber -ErrorAction Stop | Export-Csv -Path "output\($computer)_Mboard.csv" -ErrorAction Stop
+        #bios
+        if ($config.settings.bios) {
+            try {
+                $data = Get-WmiObject win32_bios @auth -ErrorAction Stop | Select-Object Version, Manufacturer, releasedate -ErrorAction Stop 
+                "{0},{1},{2},{3}" -f [string]$computer,  [string]$data.Version, [string]$data.Manufacturer,[string]$data.releasedate | add-content -Path "output\Bios.csv" 
+            }
+            catch {
+                write-host 'Failed to fetch bios'
+            }
         }
-        catch 
-        {
-            write-host 'Failed fetching motherboard'
+        #motherboard
+        if ($config.settings.baseBord) {
+            try {
+                $data = Get-WmiObject Win32_BaseBoard @auth -ErrorAction Stop | Select-Object Manufacturer, Product , Serialnumber -ErrorAction Stop 
+                "{0},{1},{2},{3}" -f [string]$computer,  [string]$data.Manufacturer, [string]$data.Product,[string]$data.Serialnumber | add-content -Path "output\Mboard.csv" 
+            }
+            catch {
+                write-host 'Failed fetching motherboard'
+            }
         }
-    }
-    #CPU
-    if($config.settings.proccessor){
-        try 
-        {
-            Get-WmiObject  win32_processor -ComputerName $computer  -Credential $credential -ErrorAction Stop | Select-Object Name,processortype, Numberofcores, numberoflogicalprocessors | Export-csv -Path "output\($computer)_Cpu.csv"
+        #CPU
+        if ($config.settings.proccessor) {
+            try {
+                $data = Get-WmiObject  win32_processor @auth -ErrorAction Stop | Select-Object Name, processortype, Numberofcores, numberoflogicalprocessors 
+                "{0},{1},{2},{3},{4}" -f [string]$computer,  [string]$data.Name, [string]$data.processortype,[string]$data.Numberofcores, [string]$data.numberoflogicalprocessors  | add-content -Path "output\Cpu.csv" 
+            }
+            catch {
+                write-host 'Could not fetch proccessor'
+            }
         }
-        catch 
-        {
-            write-host 'Could not fetch proccessor'
+        #ram
+        if ($config.settings.physical_memory) {
+            try {
+                $data = Get-WmiObject  win32_physicalmemory @auth -ErrorAction Stop  | Select-Object devicelocator , capacity, manufacturer, partnumber, serialnumber 
+                "{0},{1},{2},{3},{4},{5}" -f [string]$computer,  [string]$data.devicelocator, [string]$data.capacity,[string]$data.manufacturer, [string]$data.partnumber ,[string]$data.serialnumber  | add-content -Path "output\Ram.csv" 
+            }
+            catch {
+                write-host 'Could not fetch RAM'
+            }
         }
-    }
-    #ram
-    if($config.settings.physical_memory){
-        try 
-        {
-            Get-WmiObject  win32_physicalmemory -ComputerName $computer  -Credential $credential -ErrorAction Stop  | Select-Object devicelocator , capacity,manufacturer, partnumber, serialnumber | export-csv -Path "output\($computer)_Ram.csv"
+        #HDD
+        if ($config.settings.diskdrive) {
+            try {
+                $data = Get-WmiObject Win32_DiskDrive @auth -ErrorAction Stop | Select-Object Manufacturer, Model, SerialNumber, Size
+                "{0},{1},{2},{3},{4}" -f [string]$computer,  [string]$data.Manufacturer, [string]$data.Model,[string]$data.SerialNumber, [string]$data.Size  | add-content -Path "output\Hdd.csv"  
+            }
+            catch {
+                write-host 'Could not fetch HDD'
+            }
         }
-        catch 
-        {
-            write-host 'Could not fetch RAM'
+        #logic disks
+        if ($config.settings.logicaldisk) {
+            try {
+                $data = Get-WmiObject win32_logicaldisk @auth -ErrorAction Stop | Select-Object DeviceID, Filesystem, size, freespace 
+                "{0},{1},{2},{3},{4}" -f [string]$computer,  [string]$data.DeviceID, [string]$data.Filesystem,[string]$data.size, [string]$data.freespace  | add-content -Path "output\Hddlogic.csv"  
+            }
+            catch {
+                write-host 'Could not fetch Loguic disks'
+            }
         }
-    }
-    #HDD
-    if($config.settings.diskdrive){
-        try 
-        {
-            Get-WmiObject Win32_DiskDrive -ComputerName $computer  -Credential $credential -ErrorAction Stop | Select-Object Manufacturer, Model,SerialNumber,Size | Export-Csv -Path "output\($computer)_hdd.csv"
+        #windows updates
+        if ($config.settings.windows_update) {
+            try {
+                $data = Get-WmiObject win32_quickfixengineering @auth -ErrorAction Stop  | Select-Object description, hotfixid 
+                "{0},{1},{2},{3},{4}" -f [string]$computer,  [string]$data.description, $data.hotfixid | add-content -Path "output\Wupdate.csv" 
+            }
+            catch {
+                write-host 'Failed to fetch Windows Updates'
+                write-host $_
+            }
         }
-        catch 
-        {
-            write-host 'Could not fetch HDD'
+        #Software
+        if ($config.settings.software) {
+            try {
+                $data = Get-WmiObject win32_product @auth -ErrorAction Stop | Select-Object name 
+                "{0},{1}" -f [string]$computer,  [string]$data.name | add-content -Path "output\Software.csv" 
+            }
+            catch {
+                write-host 'Failed to fetch Softwares'
+            }
         }
-    }
-    #logic disks
-    if($config.settings.logicaldisk){
-        try 
-        {
-            Get-WmiObject win32_logicaldisk -ComputerName $computer  -Credential $credential -ErrorAction Stop | Select-Object DeviceID, Filesystem, size, freespace | Export-Csv -Path "output\($computer)_hddlogic.csv"
-        }
-        catch 
-        {
-            write-host 'Could not fetch Loguic disks'
-        }
-    }
-    #windows updates
-    if($config.settings.windows_update){
-        try 
-        {
-            Get-WmiObject win32_quickfixengineering -ComputerName $computer  -Credential $credential -ErrorAction Stop  | Select-Object description,hotfixid | Export-Csv -Path "output\($computer)_wupdate.csv"
-        }
-        catch 
-        {
-            write-host 'Failed to fetch Windows Updates'
-        }
-    }
-    #Software
-    if($config.settings.software){
-        try 
-        {
-            Get-WmiObject win32_product -ComputerName $computer  -Credential $credential -ErrorAction Stop | Select-Object name | Export-Csv -Path "output\($computer)_mjukvara.csv"    
-        }
-        catch 
-        {
-            write-host 'Failed to fetch Softwares'
-        }
-    }
-    #ip
-    if($config.settings.ip) {
-        try 
-            {   
-                $path = "output/($computer)_IP.csv"
-                if(test-path -Path $path -eq false){
-                    New-Item -Path "output/" -Name "($computer)_IP.csv" -ItemType "file"
-                }
-                $data = Get-WmiObject win32_networkadapterconfiguration -ComputerName $computer  -Credential $credential -ErrorAction Stop 
-                foreach($item in $data){
-                    "{0},{1}" -f $data[$i.ipaddress],$data[$i.defaultgateway] | add-content -Path "output\($computer)_IP.csv"
+        #ip
+        if ($config.settings.ip) {
+            try {   
+                $data = Get-WmiObject win32_networkadapterconfiguration @auth -ErrorAction Stop 
+                $i = 0
+                foreach ($item in $data) {
+                    "{0},{1},{2},{3}" -f [string]$data[$i].ipaddress, [string]$data[$i].DefaultIPGateway, [string]$data[$i].dnsdomain, [string]$data[$i].dhcpenabled  | add-content -Path "output\IP.csv"                    
+                    $i = $i + 1
                 }
             }
-        catch 
-            {
+            catch {
                 write-host 'failed to fetch ip'
             }
-    
-    
-    }
-    #-computer COMPUTERNAME
+        }
+        #-computer COMPUTERNAME
 
-    write-host $computer ' Done'
+        write-host $computer ' Done'
+    }
+    else {
+        write-host $computer " is not online"
+    }
 }
 #-computer COMPUTERNAME
